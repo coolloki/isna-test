c782535bfaa92e5954cd06df8f9d05432d6331e5
cool_loki <cooool.loki@gmail.com>
2022-08-08 21:06:58 +0600

    Refactored ISNA-259 test

--- cypress/integration/ISNA_LS/API/ISNA-259.spec.js
+++ cypress/integration/ISNA_LS/API/ISNA-259.spec.js
@@ -2,24 +2,27 @@ import Payment from '../../../fixtures/ISNA_LS/objects/Payment'
 import testDataSource from '../../../fixtures/ISNA_LS/tests_data/ISNA-259_test_data.json'
 import Commons from '../../../fixtures/COMMON/Commons'
 
+const payment = new Payment()
+//external_code и генерируются заранее для всех кейсов
+testDataSource.forEach(iteration =>{
+    const external_code = new Commons().generateUUID()
+    iteration.payment.external_code = external_code
+})
+
 describe("ISNA-259. Обработка Онлайн платежей с ИИН в назначении платеж", () => {
+
+    before(() => {
+
+        testDataSource.forEach(caseData =>{
+            payment.sendPaymentToGTM(caseData.payment)
+        })
+        cy.wait(4500)
+    })
 
     testDataSource.forEach(caseData => {
         it(caseData.testTittle, function () {
-
-            const payment = new Payment()
-            const external_code = new Commons().generateUUID()
-            
-            const paymentParam = caseData.payment
-
-            paymentParam.external_code = external_code
             const paymentResult = caseData.result
-            
-            payment.sendPaymentToGTM(paymentParam)
-
-            cy.wait(4000)
-            
-            payment.checkPaymentByExternalCode(external_code, paymentResult)
+            payment.checkPaymentByExternalCode(caseData.payment.external_code, paymentResult)
         })
     })
 })
